92050dc82446491c9264699c8b4a1beaa0433812
Klaus Dieter Kupper <klausdk1999@gmail.com>
2024-04-13 17:43:10 -0300

    feat: customer form

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,14 @@
-import { Routes } from '@angular/router';
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { LoginComponent } from './login/login.component'; // Import LoginComponent
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: 'login', component: LoginComponent },
+  { path: '', redirectTo: '/login', pathMatch: 'full' }, // Redirect to /login as the default route
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule],
+})
+export class AppRoutingModule {}
---
+++ src/app/login/login.component.ts
@@ -0,0 +1,26 @@
+import { Component } from '@angular/core';
+import { AuthService } from '../auth.service';
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.css'],
+})
+export class LoginComponent {
+  username: string = '';
+  password: string = '';
+
+  constructor(private authService: AuthService) {}
+
+  onSubmit() {
+    this.authService.login(this.username, this.password).subscribe(
+      (data) => {
+        console.log('Login successful', data);
+        alert('Login successful');
+      },
+      (error) => {
+        console.error('Login failed', error);
+        alert('Login failed');
+      }
+    );
+  }
+}
